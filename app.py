6edc247bb291903272b74499e778e252e17af03e
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-15 13:33:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,32 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 
-# Sample data for the table
-data = {
-    'Image': ['Image 1', 'Image 2', 'Image 3'],
-    'Path': ['path_to_image1.jpg', 'path_to_image1.jpg', 'path_to_image1.jpg']
-}
-df = pd.DataFrame(data)
+st.markdown("### Upload a video file:")
+uploaded_file = st.file_uploader("Choose a video file", type=['mp4', 'avi'])
 
-# Function to display selected image
-def display_image(image_path):
-    img = Image.open(image_path)
-    st.image(img, caption='Selected Image', use_column_width=True)
+if uploaded_file is not None:
 
-# Streamlit app
-def main():
-    st.title('Image Selector App')
-
-    # Display the table
-    st.dataframe(df)
-
-    # Selectbox to choose the image
-    selected_frame = st.selectbox('Select Image', df['Path'])
+    # Sample data for the table
+    data = {
+        'Image': ['Image 1', 'Image 2', 'Image 3'],
+        'Path': ['path_to_image1.jpg', 'path_to_image1.jpg', 'path_to_image1.jpg']
+    }
+    df = pd.DataFrame(data)
+    
+    # Function to display selected image
+    def display_image(image_path):
+        img = Image.open(image_path)
+        st.image(img, caption='Selected Image', use_column_width=True)
+    
+    # Streamlit app
+    def main():
+        st.title('Image Selector App')
+    
+        # Display the table
+        st.dataframe(df)
+    
+        # Selectbox to choose the image
+        selected_frame = st.selectbox('Select Image', df['Path'])
 
     # Display the selected image
     display_image(selected_frame)
