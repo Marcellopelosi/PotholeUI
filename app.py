3e41a6740ac7158b4f5dcb42e7c4a849870d2731
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-15 14:06:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-from Fundamentals import results_elaboration
 import ultralytics
 from ultralytics import YOLO
+from Fundamentals import results_elaboration
+
 
 model_path = "/best_pothole.pt"
 video_path = "/temp_video.mp4"
