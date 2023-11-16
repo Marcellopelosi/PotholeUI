35ff44982d5ed06aa1ea2e56969d22045fb10873
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-16 18:07:48 +0100

    Update app_beta.py

--- app_beta.py
+++ app_beta.py
@@ -2,11 +2,11 @@ import streamlit as st
 from PIL import Image
 import torch
 
-# Load an image from file or URL
-image = Image.open("path_to_image1.jpg")  # Replace "path_to_your_image.jpg" with your image file's path
+# # Load an image from file or URL
+# image = Image.open("path_to_image1.jpg")  # Replace "path_to_your_image.jpg" with your image file's path
 
-# Display the image
-st.image(image, caption='Your Image', use_column_width=True)
+# # Display the image
+# st.image(image, caption='Your Image', use_column_width=True)
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
