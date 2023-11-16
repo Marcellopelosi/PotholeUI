da5bdcf7f0eca07e85e37de74cf51a290fea18e9
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-16 18:01:05 +0100

    Update app_beta.py

--- app_beta.py
+++ app_beta.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Load an image from file or URL
-image = Image.open("image.jpg")  # Replace "path_to_your_image.jpg" with your image file's path
+image = Image.open("path_to_image1.jpg")  # Replace "path_to_your_image.jpg" with your image file's path
 
 # Display the image
 st.image(image, caption='Your Image', use_column_width=True)
